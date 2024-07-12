3bce852794ae48cbec5e7cf59c0f1e0481c819e4
Spectra-cularecho <pawanpanti9@gmail.com>
2024-07-12 21:04:18 +0545

    first commit

---
+++ keyconfig.ts
@@ -0,0 +1,2 @@
+export const PRIVATE_KEY:string="b59d8f5571206720d3612127cdffe1fdc8860ab54967408bd37aad04ea470a42"
+export const PRIVATE_KEY2:string="e2711f2862e986fd1292d52a96aaeaadf2b78017661f9bf74d3bfc414f526f77"
